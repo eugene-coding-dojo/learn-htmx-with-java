43a3ee1bdcbcdd38c6931af239f032748f7b01fe
Евгений Терехов <nergal@psu.ru>
2024-02-28 08:52:27 +0400

    chore(init): project template from Spring Initializer

---
+++ src/main/java/ru/perm/nergal/learnhtmx/LearnHtmxApplication.java
@@ -0,0 +1,13 @@
+package ru.perm.nergal.learnhtmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnHtmxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnHtmxApplication.class, args);
+    }
+
+}
